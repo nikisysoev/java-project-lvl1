240708ebdf16c75f1eff3427c04401bb2df242b1
Nikita Sysoev <nikisysoev@gmail.com>
2021-12-23 22:35:09 +0700

    Step7

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 public class App {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -26,6 +28,9 @@ public class App {
             case "3":
                 Cli.greet();
                 Calc.start();
+            case "4":
+                Cli.greet();
+                GCD.start();
             default:
                 break;
         }
---
+++ src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,24 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
+public class GCD {
+    public static void start() {
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        var i = 0;
+        while (i < Engine.NUMBER_OF_CHECKS && !Engine.getIsUserMakeMistake()) {
+            int randomNumber1 = Engine.makeRandomNumber(Engine.RANGE_OF_RANDOM_NUMBERS);
+            int randomNumber2 = Engine.makeRandomNumber(Engine.RANGE_OF_RANDOM_NUMBERS);
+            String correctAnswer = Engine.getCorrectAnswer(randomNumber1, randomNumber2);
+            System.out.println("Question: " + randomNumber1 + " " + randomNumber2);
+            System.out.print("Your answer: ");
+            Cli.readInput();
+            String answer = Cli.getInput();
+            Engine.checkAnswersOfUser(correctAnswer, answer);
+            i++;
+        }
+        Engine.checkIsUserMakeMistake();
+    }
+}
