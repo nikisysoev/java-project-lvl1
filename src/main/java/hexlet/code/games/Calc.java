cfe5cd45e1b845b3ba7bae06d7e83b6ae941db67
Nikita Sysoev <nikisysoev@gmail.com>
2021-12-23 21:32:20 +0700

    Step6

---
+++ src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,25 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
+public class Calc {
+    public static void start() {
+        System.out.println("What is the result of the expression?");
+
+        var i = 0;
+        while (i < Engine.NUMBER_OF_CHECKS && !Engine.getIsUserMakeMistake()) {
+            int randomNumber1 = Engine.makeRandomNumber(Engine.RANGE_OF_RANDOM_NUMBERS);
+            int randomNumber2 = Engine.makeRandomNumber(Engine.RANGE_OF_RANDOM_NUMBERS);
+            String randomOperation = Engine.makeRandomOperation(Engine.RANGE_OF_RANDOM_NUMBERS);
+            String correctAnswer = Engine.getCorrectAnswer(randomNumber1, randomNumber2, randomOperation);
+            System.out.println("Question: " + randomNumber1 + " " + randomOperation + " " + randomNumber2);
+            System.out.print("Your answer: ");
+            Cli.readInput();
+            String answer = Cli.getInput();
+            Engine.checkAnswersOfUser(correctAnswer, answer);
+            i++;
+        }
+        Engine.checkIsUserMakeMistake();
+    }
+}
