34ce307a2899a25433ffab879cfd14a973f3c3ce
nikita <nikisysoev@gmail.com>
2022-02-04 01:18:25 +0700

    change Settings

--- src/main/java/hexlet/code/Settings.java
+++ src/main/java/hexlet/code/Settings.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class Settings {
-    public static final int ATTEMPTS = 10;
+    public static final int ATTEMPTS = 3;
     public static final int RANGE_OF_RANDOM_NUMBERS = 100;
 
     public static int randomNum(int rangeOfRandomNumbers) {
